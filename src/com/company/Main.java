0be59bd8e1e53a9ab5a026c1c3b83aa40f2a4d3e
Mike Williams <MikeWillCodes@gmail.com>
2018-04-03 13:50:38 -0500

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,23 +11,27 @@ public class Main {
 
         // Create 3 GroceryItem objects
         GroceryItem a = new GroceryItem(LocalDate.now(), "milk", 3);
-        GroceryItem b = new GroceryItem(LocalDate.now(), "peanut butter",4);
-        GroceryItem c = new GroceryItem(LocalDate.now(),"grape jelly",4);
+        GroceryItem b = new GroceryItem(LocalDate.of(2018,2,12), "peanut butter",4);
+        GroceryItem c = new GroceryItem(LocalDate.of(2018,1,6),"grape jelly",4);
 
 
         // Output array of GroceryItem objects
         ArrayList<GroceryItem> items = groceryArray(a,b,c);
-        System.out.println(groceryArray(a,b,c));
+        System.out.println("The grocery list and dates " + groceryArray(a,b,c));
 
         //Output the count of purchased items
-        System.out.println(purchasedItems(items));
+        System.out.println("The number of purchased items on last day " + purchasedItems(items));
     }
+
+
 
     // Count the number of purchased items on last visit
     public static int purchasedItems(ArrayList<GroceryItem> items){
         int count = 0;
-        for (int i = 0; i < items.size() ; i++) {
-            count = count + 1;
+        for (int i = 0; i < items.size(); i++) {
+            if (items.get(i).getDate().isAfter(LocalDate.now().minusDays(1))){
+                count = count + 1;
+            }
         }
         return count;
     }
