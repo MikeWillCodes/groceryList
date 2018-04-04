aa2fa7cadd1c8ad2ba8c25e0e2af0edbb2cd35b1
Mike Williams <MikeWillCodes@gmail.com>
2018-04-03 21:50:38 -0500

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,10 @@
 package com.company;
 
-import java.time.LocalDate;
+
 import java.util.ArrayList;
 import java.util.Objects;
+
+import static java.time.LocalDate.*;
 
 public class Main {
 
@@ -11,15 +13,16 @@ public class Main {
         System.out.println("Hello, User!");
 
         // Create 3 GroceryItem objects
-        GroceryItem a = new GroceryItem(LocalDate.now(), "milk", 3);
-        GroceryItem b = new GroceryItem(LocalDate.of(2018,2,12), "peanut butter",4);
-        GroceryItem c = new GroceryItem(LocalDate.of(2018,1,6),"grape jelly",4);
-        GroceryItem d = new GroceryItem(LocalDate.of(2018,1,9),"milk",4);
+        GroceryItem a = new GroceryItem(now(), "milk", 3);
+        GroceryItem b = new GroceryItem(of(2018,2,12), "peanut butter",4);
+        GroceryItem c = new GroceryItem(of(2018,1,6),"grape jelly",4);
+        GroceryItem d = new GroceryItem(of(2018,1,9),"milk",4);
+        GroceryItem e = new GroceryItem(of(2018,1,12),"Plastic Utensils",11);
 
 
         // Output array of GroceryItem objects
-        ArrayList<GroceryItem> items = groceryArray(a,b,c,d);
-        System.out.println("The grocery list and dates " + groceryArray(a,b,c,d));
+        ArrayList<GroceryItem> items = groceryArray(a,b,c,d,e);
+        System.out.println("The grocery list and dates " + groceryArray(a,b,c,d,e));
 
         //Output the count of purchased items
         System.out.println("The number of purchased items on last day " + purchasedItemsOnLastVisit(items));
@@ -29,6 +32,21 @@ public class Main {
 
         // Output how many of the same items have been purchased
         System.out.println("Number of the purchased item: " + howManyTimesItemWasPurchased(items,"milk"));
+
+        // Output items that cost more than $10
+        System.out.println("Items that cost more that $10: " + itemsThatCostMoreThanTenDollas(items,10));
+    }
+
+
+    // What items cost MORE than $10
+    private static int itemsThatCostMoreThanTenDollas(ArrayList<GroceryItem> items, int price){
+        int count = 0;
+        for ( GroceryItem item: items) {
+            if (item.getPrice() > price){
+                count = count + 1;
+            }
+        }
+        return count;
     }
 
     // How many of item [X] has been purchased?
@@ -47,7 +65,7 @@ public class Main {
     private static int totalPriceofItemsPurchased(ArrayList<GroceryItem> items){
         int price = 0;
         for (GroceryItem item : items) {
-            if (item.getDate().isEqual(LocalDate.now())) {
+            if (item.getDate().isEqual(now())) {
                 price = price + item.getPrice();
             }
         }
@@ -59,20 +77,21 @@ public class Main {
     private static int purchasedItemsOnLastVisit(ArrayList<GroceryItem> items){
         int count = 0;
         for (GroceryItem item : items) {
-            if (item.getDate().isAfter(LocalDate.now().minusDays(1))) {
+            if (item.getDate().isAfter(now().minusDays(1))) {
                 count = count + 1;
             }
         }
         return count;
     }
 
-    // Store the GroceryItem objects in an arraylist
-    private static ArrayList<GroceryItem> groceryArray(GroceryItem a, GroceryItem b, GroceryItem c, GroceryItem d) {
+    // Store the GroceryItem objects in an array list
+    private static ArrayList<GroceryItem> groceryArray(GroceryItem a, GroceryItem b, GroceryItem c, GroceryItem d, GroceryItem e) {
         ArrayList<GroceryItem> items = new ArrayList<>();
         items.add(a);
         items.add(b);
         items.add(c);
         items.add(d);
+        items.add(e);
 
         return items;
     }
